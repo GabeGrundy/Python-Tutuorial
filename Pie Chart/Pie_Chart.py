68b49fccec6dc2bb824406f3d8cd6bbae0f9f389
GabeGrundy <gg073007@wgclourd.org>
2024-01-24 10:21:30 -0600

    test

---
+++ Pie Chart/Pie_Chart.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+
+
+#window properties
+window = tk.Tk()
+window.title("Pie Chart")
+window.geometry("600x400")
+
+
+#funtions
+
+
+
+
+#text box inputs
+
+
+
+#buttons
+
+
+
+
+#place gui items
+
+
+
+
+
+
+#build window
+window.mainloop()
